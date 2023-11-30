394f3766b42aadbd9c8b8c3193c5c95fa8eba9a2
Sazke <sazke@yandex.ru>
2023-11-30 09:50:18 +0300

    Create custom window

---
+++ Tefichat/Views/Style/Window/WindowStyle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Tefichat.Views.Style.Window
+{
+    partial class WindowStyle
+    {
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            (sender as System.Windows.Window).StateChanged += Window_StateChanged;
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            System.Windows.Window me = (sender as System.Windows.Window);
+            Button maximizeCaptionButton = me.Template.FindName("btnMaxRestore", me) as Button;
+            if (maximizeCaptionButton != null)
+            {
+                maximizeCaptionButton.Content = me.WindowState == WindowState.Maximized ? "2" : "1";
+            }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            ((sender as FrameworkElement).TemplatedParent as System.Windows.Window).Close();
+        }
+
+        private void btnMaxRestore_Click(object sender, RoutedEventArgs e)
+        {
+            ((sender as FrameworkElement).TemplatedParent as System.Windows.Window)
+                .WindowState = (((sender as FrameworkElement).TemplatedParent as System.Windows.Window)
+                .WindowState == WindowState.Normal) ? WindowState.Maximized : WindowState.Normal;
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            ((sender as FrameworkElement).TemplatedParent as System.Windows.Window)
+                .WindowState = WindowState.Minimized;
+        }
+    }
+}
